e0cb10bcb3efdd28c70a0f6140f6bfd1fc9060f3
Håkan Andersson <hakan.hemma@gmail.com>
2021-01-28 11:57:34 +0100

    Added analog pins as digital.

--- src/main.cpp
+++ src/main.cpp
@@ -9,41 +9,35 @@
 EventController * events = new EventController();
 
 // Input pin for button.
-DigitalPin * pinEmergencyStop = new DigitalPin(1, INPUT);
-DigitalPin * pinButton = new DigitalPin(2, INPUT);
-DigitalPin * pinAcAttached = new DigitalPin(3, INPUT);
+DigitalPin * pinEmergencyStop = new DigitalPin(2, INPUT);
+DigitalPin * pinButton = new DigitalPin(3, INPUT);
+DigitalPin * pinAcAttached = new DigitalPin(4, INPUT);
+
+// Digital Out pins
+DigitalPin * pinLed = new DigitalPin(10, OUTPUT);
+DigitalPin * pinRelayMain = new DigitalPin(11, OUTPUT);
+DigitalPin * pinTimedOn = new DigitalPin(12, OUTPUT);
 
 // Display Pins
-DigitalPin * displayEnable = new DigitalPin(4, OUTPUT);
-DigitalPin * displayData1 = new DigitalPin(5, OUTPUT);
-DigitalPin * displayData2 = new DigitalPin(6, OUTPUT);
-DigitalPin * displayData3 = new DigitalPin(7, OUTPUT);
-DigitalPin * displayData4 = new DigitalPin(8, OUTPUT);
+// Analogue as digital pins A0-A5 (pin 14-19)
+DigitalPin * displayEnable = new DigitalPin(14, OUTPUT);
+DigitalPin * displayData1 = new DigitalPin(15, OUTPUT);
+DigitalPin * displayData2 = new DigitalPin(16, OUTPUT);
+DigitalPin * displayData3 = new DigitalPin(17, OUTPUT);
+DigitalPin * displayData4 = new DigitalPin(18, OUTPUT);
 
-// LED pins
-DigitalPin * pin10 = new DigitalPin(10, OUTPUT);
-DigitalPin * pin11 = new DigitalPin(11, OUTPUT);
-DigitalPin * pin12 = new DigitalPin(12, OUTPUT);
-DigitalPin * pin13 = new DigitalPin(13, OUTPUT);
 
 void setup() 
 {
   Serial.begin(9600);
- 
-  // Just do a continous blink every two seconds.
-  Event * event = new Event(Handler::SampleFunction, 2000UL, pin13);
-  events->InsertLast(event);
-  
-  // Button is on PIN2 and the button handler manage Pin10-12 which is sent along as Referenced data and is used in event-handler for ButtonEvent.
-  DigitalPin * pins[] = { pin10, pin11, pin12 };
-  ButtonEvent * bEvent = new ButtonEvent(pinButton, Handler::HandleButtonPress, Handler::HandleButtonLongPress, Handler::HandleButtonDoublePress, pins);
+
+  // Button is on PIN2 and the button handler manage Pin10-12 which is sent along as referenced data and is used in event-handler for ButtonEvent.
+  DigitalPin * pins[] = { pinLed, pinRelayMain, pinTimedOn };
+  ButtonEvent * bEvent = new ButtonEvent(pinButton, Handler::HandleButtonPress, Handler::HandleButtonLongPress, Handler::HandleButtonDoublePress, Handler::HandleButtonDownPress, NULL, pins);
   events->InsertLast(bEvent);
 
-  // Emulate morse code SOS on PIN12 where one perod is 180ms. Notice last time is set to 0 to end the array.
-  const unsigned long p = 120;
-  unsigned long times[] = { p*7, p, p, p, p, p, p, p*3, p, p*3, p, p*3, p, p, p, p, p, p, 0 };
-  TimerEvent * tEvent = new TimerEvent("SOS", times, false, Handler::TogglePinForSosMessage, pin12);
-  events->InsertLast(tEvent);
+  // Controller On Led
+  pinLed->ToggleValue();
 
   Serial.println("Initialized");
 }
