2d4a796412b61d6c5821b9b3a45802ea879a0e77
Håkan Andersson <hakan.hemma@gmail.com>
2021-02-16 09:40:15 +0100

    GetDigitalValue implemented for states.

--- lib/Eventfully/src/AnaloguePin.h
+++ lib/Eventfully/src/AnaloguePin.h
@@ -7,19 +7,23 @@
 class AnaloguePin : public Pin
 {
     private:
-        float _maxVoltage = 5.0;
+        float _maxVoltage;
 
     protected:
-        double _value;
+        double _value, _voltageLow, _voltageHigh;
         long _resistor1, _resistor2;
 
     public:
         AnaloguePin(int pin, int direction);
+        AnaloguePin(int pin, int direction, double voltageLow, double voltageHigh);
         AnaloguePin(int pin, unsigned long resistor1, unsigned long resistor2);
         AnaloguePin(int pin, double measureMax, unsigned long resistor1, unsigned long resistor2);
+        AnaloguePin(int pin, unsigned long resistor1, unsigned long resistor2, double voltageLow, double voltageHigh);
+        AnaloguePin(int pin, double measureMax, unsigned long resistor1, unsigned long resistor2, double voltageLow, double voltageHigh);
 
         double GetVoltage();
         void SetVoltage(double voltage);
+        virtual bool GetDigitalValue();
 };
 
 #endif
